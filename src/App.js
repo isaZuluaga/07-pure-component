ef261e71760704502d42f152069cab50b913b3c2
juvasquezg <juvasquez88@gmail.com>
2020-04-21 18:37:56 -0500

    feat: add React.js pureComponent example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Task extends PureComponent {
+  render() {
+    const { t } = this.props
+    console.log(t)
+    return (
+      <div>
+        <span>X</span>
+        {t.value}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    tasks: []
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const value = e.target[0].value
+    const id = Math.random().toString(16)
+    const task = { value, id }
+    this.setState(state => ({
+      tasks: [
+        ...state.tasks,
+        task
+      ]
+    }))
+
+    // clean input
+    e.target[0].value = ''
+  }
+
+  render() {
+    const { tasks } = this.state
+    return (
+      <div className="App">
+        <h1 className="title">shouldComponentUpdate</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="task" id="task" placeholder="task input" />
+          <button className="btn">Add task</button>
+        </form>
+        <div className="taskList">
+          {tasks.map(t => (<Task
+              key={t.id}
+              t={t}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
